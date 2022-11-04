877cd3d346d2cd7a1ca1bc2bd0b293916fc6abb5
KatushaKononova56 <83833827+KatushaKononova56@users.noreply.github.com>
2022-11-04 15:47:36 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random_array();
+        }
+
+        static void Random_array(){
+            int[] array = new int[8];
+            for(int i=0;i<8;i++){
+                array[i]=new Random().Next(0,10);
+                Console.Write($"{array[i]} ");
+            }
+        }
+    }
+}
