4a6688264472c01961462a5afcb8725e555a2a24
KatushaKononova56 <83833827+KatushaKononova56@users.noreply.github.com>
2022-11-04 15:38:30 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+using System;
+
+namespace task2
+{
+    class Program
+    {
+        public static int num=0;
+        
+        static void Main(string[] args)
+        {
+            int sum = Enter_number_and_sum();
+            Console.WriteLine("Сумма равна " + Convert.ToString(sum));
+        }
+
+        static int Enter_number_and_sum(){
+            Console.WriteLine("Введите целое число");
+            num=Convert.ToInt32(Console.ReadLine());
+            int sum_f=0;
+            while(num>0){
+                sum_f+=num%10;
+                num/=10;
+            }
+            return sum_f;
+        }
+    }
+}
