2f868e16a5460d8ce4642d0de649ec7f7e3e4ab7
KatushaKononova56 <83833827+KatushaKononova56@users.noreply.github.com>
2022-11-04 15:33:28 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+using System;
+
+
+
+namespace task1
+{
+    class Program
+    {
+        public static int numA=0;
+        public static int numB=0;
+        static void Main(string[] args)
+        {
+            Enter_Two_number();
+            Console.WriteLine("A^B=" + Convert.ToString(Math.Pow(numA,numB)));
+        }
+
+        static void Enter_Two_number(){
+            Console.WriteLine("Введите целые числа A и B");
+            numA=Convert.ToInt32(Console.ReadLine());
+            numB=Convert.ToInt32(Console.ReadLine());
+            return;
+        }
+    }
+}
